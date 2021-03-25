d0f57cfd2044953f0e9e0197e22160374be26c60
senzxita <zannurita@gmail.com>
2021-03-25 15:51:13 +0100

    Created a bash script to read /etc/passwd file

--- Week2/userinfo.sh
+++ Week2/userinfo.sh
@@ -5,10 +5,7 @@ do
 echo "Username:$f1 GID:$f4 Homedir:$f6 shell:$f7" > userinfo.txt
 done < /etc/passwd
 
-
-
-
-
+#Pushing changes to the respository on GitHub
 git pull origin main
 git add .
 git commit -m "Created a bash script to read /etc/passwd file "
