6217da857bf14161673b60d43d090a1680bd302d
senzxita <zannurita@gmail.com>
2021-03-25 15:05:39 +0100

    Created a bash script to read /etc/passwd file

--- Week2/userinfo.sh
+++ Week2/userinfo.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 while IFS=: read -r f1 f2 f3 f4 f5 f6 f7
 do
-echo Username:$f1 GID:$f4 Homedir:$f6 shell:$f7
+Userinfo='echo Username:$f1 GID:$f4 Homedir:$f6 shell:$f7'
 done < /etc/passwd
+
+nano Userinfo.txt
+echo Userinfo
+
 
 cd home/cloudqueen/SCA-Tasks
 git add .
