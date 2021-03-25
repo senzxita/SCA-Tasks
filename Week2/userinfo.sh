3ba49f2746bc95bcf84785bf9855f15a7cdbab4d
senzxita <zannurita@gmail.com>
2021-03-25 15:00:54 +0100

    Created a bash script to read /etc/passwd file

---
+++ Week2/userinfo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while IFS=: read -r f1 f2 f3 f4 f5 f6 f7
+do
+echo Username:$f1 GID:$f4 Homedir:$f6 shell:$f7
+done < /etc/passwd
+
+cd home/cloudqueen/SCA-Tasks
+git add .
+git commit -m "Created a bash script to read /etc/passwd file "
+git push origin main
+
+
